5074b9339388cc8e46a634cf1ae91336c1b6dfe1
John Freeman <john.freeman.opensource@gmail.com>
2018-08-29 15:18:33 +0000

    Added script to update testing documentation
    For Molecule 2 changes.

---
+++ update-testing-doc.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+set -e
+
+hub_download_url=$(curl --silent -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/github/hub/releases \
+        | jq --raw-output '.[0].assets[] | select(.name | test("hub-linux-amd64-[0-9.]+.tgz")) | .browser_download_url')
+
+
+mkdir --parents '.tmp'
+
+(cd '.tmp' && set -x | curl --silent --location "$hub_download_url" | tar xvz --strip=2 --wildcards '*/bin/hub')
+
+repos=($(curl --silent -H 'Accept: application/vnd.github.v3+json' https://api.github.com/users/gantsign/repos \
+        | jq --raw-output '.[] | select(.archived == false) | .name | select(. | test("ansible-role-.*"))'))
+repos+=($(curl --silent -H 'Accept: application/vnd.github.v3+json' https://api.github.com/users/gantsign/repos \
+        | jq --raw-output '.[] | select(.archived == false) | .name | select(. | test("ansible_role_.*"))'))
+
+commit_msg="\
+Updated testing instructions
+
+There are differences with Molecule 2.
+"
+
+branch_name="testing-doc"
+
+changed_file='README.md'
+
+update_files() {
+    before='\QTo run the role (i.e. the `tests/test.yml` playbook), and test the results\E\n\Q(`tests/test_role.py`), execute the following command from the project root\E\n\Q(i.e. the directory with `molecule.yml` in it):\E'
+    after='To test this role run the following command from the project root:'
+    (set -x && perl -i -p0e "s|$before|$after|ms" README.md)
+}
+
+print_banner() {
+    echo ''
+    echo '************************************************************'
+    echo "Processing: $1"
+    echo '************************************************************'
+    echo ''
+}
+
+update_repo() {
+    update_files
+
+    if ! (git status | grep --quiet "$changed_file"); then
+        echo 'Unable to proceed: no changes'
+        return 0
+    fi
+
+    if ! (set -x && git checkout -b "$branch_name"); then
+        echo 'Unable to proceed: error creating branch'
+        return 0
+    fi
+
+    if ! (set -x && git add --verbose .); then
+        echo 'Unable to proceed: unable to add file'
+        return 0
+    fi
+
+    if ! (set -x && git commit --message="$commit_msg"); then
+        echo 'Unable to proceed: commit failed'
+        return 0
+    fi
+
+    if ! (set -x && ../hub pull-request --push --message="$commit_msg"); then
+        echo 'Unable to proceed: failed to create pull request'
+        return 0
+    fi
+}
+
+process_repo() {
+    repo="$1"
+
+    print_banner "$repo"
+    (set -x && rm -rf "$repo")
+    if ! (set -x && git clone --depth 1 "https://github.com/gantsign/$repo"); then
+        echo 'Unable to proceed: failed to clone'
+        return 0
+    fi
+    (cd "$repo" && update_repo)
+}
+
+for repo in "${repos[@]}"; do
+    (cd '.tmp' && process_repo "$repo")
+done
+
+print_banner "Finished"
