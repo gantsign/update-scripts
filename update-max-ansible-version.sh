47eb75771f39e8297c23421e71fe99ba86ddf4a9
John Freeman <john.freeman.opensource@gmail.com>
2019-05-22 21:42:40 +0100

    Updated update-max-ansible-version.sh for version 2.8
    For the latest release.

--- update-max-ansible-version.sh
+++ update-max-ansible-version.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-new_version='2.7.0'
+new_version='2.8.0'
 
 hub_download_url=$(curl --silent -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/github/hub/releases \
         | jq --raw-output '.[0].assets[] | select(.name | test("hub-linux-amd64-[0-9.]+.tgz")) | .browser_download_url')
@@ -15,9 +15,9 @@ repos=($(curl --silent -H 'Accept: application/vnd.github.v3+json' 'https://api.
         | jq --raw-output '.[] | select(.archived == false) | .name | select(. | test("ansible.role.*"))'))
 
 commit_msg="\
-Increased upper Ansible test range to Ansible 2.7
+Increased upper Ansible test range to Ansible 2.8
 
-Ensure this role works with Ansible 2.7.
+Ensure this role works with Ansible 2.8.
 "
 
 branch_name="ansible-$new_version"
@@ -29,7 +29,7 @@ update_files() {
     if [[ ${#versions[@]} == 2 ]]; then
         max_version=${versions[1]}
 
-        if [[ $max_version != 2.7.* ]]; then
+        if [[ $max_version != 2.8.* ]]; then
             (set -x && perl -i -pe \
                 "s/MOLECULEW_ANSIBLE=\Q$max_version\E/MOLECULEW_ANSIBLE=$new_version/" .travis.yml)
             (set -x && ./moleculew wrapper-freeze --ansible $new_version)
@@ -88,6 +88,7 @@ process_repo() {
 
 for repo in "${repos[@]}"; do
     (cd '.tmp' && process_repo "$repo")
+    exit
 done
 
 print_banner "Finished"
